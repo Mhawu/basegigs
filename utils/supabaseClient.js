af24f3e8762bd9e9d78ea06c4f731d3f2020c70b
Mhawu <thwalamhawu@gmail.com>
2025-12-25 15:09:15 -0600

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://remimcazgepggxklvuan.supabase.co';
-const supabaseAnonKey = 'PASTE_YOUR_ANON_KEY_HERE';
+
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlbWltY2F6Z2VwZ2d4a2x2dWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1OTY4ODcsImV4cCI6MjA4MjE3Mjg4N30.dHAvStF7sAoImIYR_N-T_rG1O8R-6FX_4LHj2lcuPiI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
